e3a127c8c5c323de06c362cfa11dd2030286941a
Bryan <bryan.barbosa@engenharia.ufjf.br>
2021-08-22 19:00:43 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-Vocore2 GPIO
+# Vocore2 GPIO Python Library
+
+This is an Arduino inspired library for Vocore2 GPIO pins control.
+
+Made by Bryan Barbosa from [Federal University of Juiz de Fora](https://ufjf.br/) and [CREA - University of California, Berkeley](https://crea.berkeley.edu/).
+
+## Importing
+
+The library can be used simply by importing the Python file to your project.
+
+```python
+import vocoreGPIO
+```
+For an easier and more Arduino-like control, we recommend to do importation with the code line:
+```python
+from vocoreGPIO import *
+```
+
+## Usage
+The usage of the library is simple as a normal a Arduino pin control. For example, for a classic blink led:
+
+```python
+from vocoreGPIO import *
+
+pinMode(40, 'OUTPUT')
+
+while(1):
+    digitalWrite(40, 'HIGH')
+    delay(1000)
+    digitalWrite(40, 'LOW')
+    delay(1000)
+```
+
+Note that the only difference between these commands and the commands used in Arduino is the quotation marks for passing the words 'OUTPUT', 'INPUT', 'HIGH' and 'LOW' as parameters.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ vocoreGPIO.py
@@ -0,0 +1,62 @@
+'''
+This library is public domain. Enjoy!
+Based on the original C++ commands for pin control in Arduino (https://www.arduino.cc/)
+Made by Bryan Barbosa (Github: BryanCMBarbosa) from Federal University of Juiz de Fora and CREA Lab - UC Berkeley
+'''
+
+import time
+
+def pinMode(num_pin, mode):
+    pin_path = "/sys/class/gpio/gpio%d/" % num_pin
+    f_direc = file(pin_path + "direction", "w")
+    value = "out"
+
+    if mode == True or mode.lower() == "true" or mode.lower() == "1" or mode.lower() == "output" or mode.lower() == "out" or mode == 1 or mode == 1.0:
+        value = "out"
+    elif mode == False or mode.lower() == "false" or mode.lower() == "0" or mode.lower() == "input" or mode.lower() == "in" or mode == 0 or mode == 0.0:
+        value = "in"
+
+    f_direc.write(value)
+    f_direc.close()
+
+
+def digitalWrite(num_pin, mode):
+    pin_path = "/sys/class/gpio/gpio%d/" % num_pin
+    f_direc = file(pin_path + "directio", "w")
+    mode = f_direc.read()
+    f_direc.close()
+
+    if mode == "out" or mode == "out\n" or mode == "out \n":
+        value = "1"
+        f_value = file(pin_path + "value", "w")
+
+        if mode == True or mode.lower() == "true" or mode.lower() == "1" or mode.lower() == "high" or mode == 1 or mode == 1.0:
+            value = "1"
+        elif mode == False or mode.lower() == "false" or mode.lower() == "0" or mode.lower() == "low" or mode == 0 or mode == 0.0:
+            value = "0"
+            
+        f_value.write(value)
+        f_value.close()
+
+    else:
+        print("Pin {} is not in OUTPUT mode.".format(num_pin))
+
+
+def digitalRead(num_pin):
+    pin_path = "/sys/class/gpio/gpio%d/" % num_pin
+    f_value = file(pin_path + "value", "r")
+    mode = f_value.read()
+    f_value.close()
+
+    if mode == "1" or mode == "1\n" or mode == "1 \n":
+        return 1
+
+    return 0
+
+
+def delay(t):
+    time.sleep((t/1000))
+
+    
+def delayMicroseconds(t):
+    time.sleep((t/1000000))
